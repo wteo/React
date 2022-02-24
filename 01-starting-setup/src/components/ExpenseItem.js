42524746f9c4b7a58819d09c3e6b76ca4c4e81f5
wteo <wendyteo.wy@gmail.com>
2022-02-25 07:46:04 +1100

    Add extra component ExpenseDate

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,13 +1,17 @@
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css'; // usually CSS files for the function are grouped together in the same folder
+
 // components in React is just a JS function
 // function is named in large capital to tell React this is a customized HTML.
 // lower cases will be regarded as regular imported HTML elements.
-function ExpenseItem() {
+function ExpenseItem(props) { // call on the paramenter. then, the name of the attributes you have given
+
     return (
-    <div>
-        <div>March 28th 2021</div>
-        <div>
-            <h2>Car Insurance</h2>
-            <div>$294.67</div>
+    <div className="expense-item">
+        <ExpenseDate date={props.date}/>
+        <div className="expense-item__description">
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">{props.amount}</div>
         </div>
     </div>
     );
