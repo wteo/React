5240908f937f214721e22adbe46c40717d01e55e
wteo <wendyteo.wy@gmail.com>
2022-05-16 04:12:18 +0800

    Added useState to update data as per user input

--- users/src/components/Users/AddUser.js
+++ users/src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
@@ -6,17 +6,29 @@ import Button from "../UI/Button";
 
 const AddUser = props => {
 
+    const [enteredUserName, setEnteredUsername] = useState("");
+    const [enteredAge, setEnteredAge] = useState("");
+
     const addUserHandler = (event) => {
         event.preventDefault();
+        console.log(enteredUserName, enteredAge);
     };
+
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value);
+    }
+
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
+    }
 
     return (
     <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" />
+            <input id="username" type="text" onChange={usernameChangeHandler} />
             <label htmlFor="age">Age (Years)</label>
-            <input id="age" type="number" />
+            <input id="age" type="number" onChange={ageChangeHandler} />
             <Button type="submit">Add User</Button>
         </form>
     </Card>
