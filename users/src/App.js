68fea3c8fa2005e429b20238d82db84ff39e636d
wteo <wendyteo.wy@gmail.com>
2022-05-20 11:14:26 +0800

    Added wrapper components to prevent "<div> soup"

--- users/src/App.js
+++ users/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 
 import AddUser from "./components/Users/AddUser";
 import UsersList from './components/Users/UsersList';
@@ -15,11 +15,12 @@ function App() {
   };
 
   return (
-    <div>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </Fragment>
   );
 }
 
 export default App;
+
