e5d7ae294143b22162651ac98979c875c73bc96c
wteo <wendyteo.wy@gmail.com>
2022-05-23 17:47:39 +0800

    Setting up a dummy login page

---
+++ login/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+
+function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
+    // storage mechanism, a global object, built into the browser independent of React
+    // first param is the key, second, value (which must be in a string)
+    // You can find key-value pair in dev tool > Application - storage - Local Storage - localHost
+    setIsLoggedIn(true);
+  };
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    } // this would create an infinite loop. This is why we need useEffect() here.
+  
+  }, []); // an empty dependencies would only run code once.
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
