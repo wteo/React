c59bbc91e19f525557556954779a33e65acd6de4
wteo <wendyteo.wy@gmail.com>
2022-05-31 16:28:48 +0800

    Replaced context consumer with useContect() hook

--- login/src/components/MainHeader/Navigation.js
+++ login/src/components/MainHeader/Navigation.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import AuthContext from '../store/auth-context';
 import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
+
+  const context = useContext(AuthContext);
+  
   return (
-    <AuthContext.Consumer>
-      {(context) => {
-        return (
+
           <nav className={classes.nav}>
             <ul>
               {context.isLoggedIn && (
@@ -28,10 +29,6 @@ const Navigation = (props) => {
             </ul>
           </nav>
         );
-      }
-      }
-    </AuthContext.Consumer>
-  );
 };
 
 export default Navigation;
--- login/src/components/store/auth-context.js
+++ login/src/components/store/auth-context.js
@@ -3,6 +3,7 @@ import React from "react";
 const AuthContext = React.createContext({
     isLoggedIn: false
 });
-// Takes a default context but you can also include whatever context you want
+// Takes a default context but you can also include whatever context you want.
+// Context could be in string. Though, we commonly include object.
 
 export default AuthContext;
