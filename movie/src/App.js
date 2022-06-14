59416bc5a593cfe2a155df998cd3372be9cf3593
wteo <wendyteo.wy@gmail.com>
2022-06-14 11:25:13 +0800

    added sideeffect when fetching data

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -9,7 +9,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler () {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
@@ -35,7 +35,11 @@ function App() {
         setError(error.message);
       }
       setIsLoading(false);
-    }
+    }, []);
+
+    useEffect(() => {
+      fetchMoviesHandler();
+    }, [fetchMoviesHandler]);
 
   let content = <p>No movie found.</p>
 
