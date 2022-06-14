5f57b5fa79c9c3d944077bc825f1e54c0bccfb40
wteo <wendyteo.wy@gmail.com>
2022-06-14 11:51:51 +0800

    Added form to post movie data into backend server

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,68 +1,87 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
-
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
+  const url = 'https://react-http-ca67a-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json';
+
   const fetchMoviesHandler = useCallback(async () => {
+
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/')
-      
+      const response = await fetch(url);
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
-      
+
       const data = await response.json();
-    
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id          : movieData.episode_id,
-          title       : movieData.title,
-          openingText : movieData.opening_crawl,
-          releaseDate : movieData.release_date
-          };
-        });
-        setMovies(transformedMovies);
-      }
-      catch(error) {
-        setError(error.message);
-      }
-      setIsLoading(false);
-    }, []);
 
-    useEffect(() => {
-      fetchMoviesHandler();
-    }, [fetchMoviesHandler]);
+      const loadedMovies = [];
 
-  let content = <p>No movie found.</p>
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate
+          });
+      }
+  
+      setMovies(loadedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  async function addMovieHandler(movie) {
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    });
+    // not just used to fetch data but also to send data.
+    // its default http request is "get". Hence, you need to use the second parameter to change http request to a different one like "put"
+    const data = await response.json();
+    console.log(data);
+  }
+
+  let content = <p>Found no movies.</p>;
 
   if (movies.length > 0) {
-    content = <MoviesList movies={movies}/>
+    content = <MoviesList movies={movies} />;
   }
 
   if (error) {
-    content = <p>{error}</p>
+    content = <p>{error}</p>;
   }
 
   if (isLoading) {
-    content = <p>Loading...</p>
+    content = <p>Loading...</p>;
   }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        {content}
-      </section>    
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
