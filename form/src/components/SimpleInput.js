10d34370f800ec3d69271351459f0421cabe447c
wteo <wendyteo.wy@gmail.com>
2022-06-18 13:21:34 +0800

    refactored validation conditions

--- form/src/components/SimpleInput.js
+++ form/src/components/SimpleInput.js
@@ -1,34 +1,19 @@
-import { useEffect useState } from 'react';
+import { useState } from 'react';
 
 const SimpleInput = (props) => {
 
   const [enteredName, setEnteredName] = useState('');
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
-  useEffect(() => {
-    if (enteredNameIsValid) {
-      console.log('Name Input is valid!');
-    }
-  }, [enteredNameIsValid]);
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value);
-
-    if (event.target.value.trim() !== '') {
-      // put event.target.value instead of enteredName state as state is not processed immediately.
-      // Hence, not ideal if want validation at every key stroke.
-      setEnteredNameIsValid(true);
-    }
   };
 
   const nameInputBlurHandler = event => {
     setEnteredNameTouched(true);
-    
-    if (enteredName.trim() === '') {
-      setEnteredNameIsValid(false);
-    }
-
   };
 
   const formSubmissionHandler = event => {
@@ -36,17 +21,13 @@ const SimpleInput = (props) => {
     
     setEnteredNameTouched(true);
 
-    if (enteredName.trim() === '') {
-      setEnteredNameIsValid(false);
+    if (!enteredNameIsValid) {
       return;
     }
-
-    setEnteredNameIsValid(true);
     
     setEnteredName("");
+    setEnteredNameTouched(false);
   };
-
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control';
 
