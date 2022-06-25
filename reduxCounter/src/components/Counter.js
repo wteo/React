50268c55d2e853db3808bd7ba708dc64c200a998
wteo <wendyteo.wy@gmail.com>
2022-06-25 18:16:43 +0800

    Created handler functions and dispatched counter

--- reduxCounter/src/components/Counter.js
+++ reduxCounter/src/components/Counter.js
@@ -1,10 +1,14 @@
-import { useSelector, connect } from 'react-redux';
+import { useSelector, useDispatch, connect } from 'react-redux';
 // useSelector - allows you to select a specific data in the store
 // connect - used for class-based components
+// useDispatch - dispatches an action
 
 import classes from './Counter.module.css';
 
 const Counter = () => {
+
+  // Dispatch
+  const dispatch = useDispatch();
 
   // Suscription
   const counter = useSelector(state => state.counter);
@@ -13,10 +17,22 @@ const Counter = () => {
 
   const toggleCounterHandler = () => {};
 
+  const incrementHandler = () => {
+    dispatch({ type: 'increment' });
+  }
+
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' });
+  }
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
