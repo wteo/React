615ac3f757e5aeaf0f041798eab32c79e181a867
wteo <wendyteo.wy@gmail.com>
2022-06-25 16:02:34 +0800

    Refined notes/comments

--- reduxDemo/redux-demo.js
+++ reduxDemo/redux-demo.js
@@ -1,5 +1,6 @@
 const redux = require('redux');
 
+// REDUCER FUNCTION
 const counterReducer = (state = { counter: 0 }, action) => {
     // Counter will be undefined when store is first initialized.
     // This is why we need to give state parameter a default value.
@@ -7,22 +8,23 @@ const counterReducer = (state = { counter: 0 }, action) => {
         counter: state.counter + 1
     };
 };
-// reducer func always receives on two parameters: 
+// Note: 
+// reducer func always receives two parameters: 
 //      1. the old/existing state; and 
-//      Note: 
 //      2. the action that was dispatched
 // this reducer func must always return a new state object.
 // Note also that the reducer func must be a pure function.
 
+// STORE
 const store = redux.createStore(counterReducer);
-// createStore is a method that creates the central data store
+// createStore() is a method that creates the central data store
 // its parameter calls on a reducer function.
 
-// Subscription
-// this subscription function does not accept any paraementer
+// SUBSCRIPTION
+// this subscription function does not accept any parameter
 const counterSubscriber = () => {
     const latestState = store.getState();
-    // getStore is a method that will give us the latest snapshot of State after it was updated.
+    // getStore() is a method that will give us the latest snapshot of State after it was updated.
     console.log(latestState);
 };
 
@@ -30,10 +32,10 @@ store.subscribe(counterSubscriber);
 // another method, which makes Redux:
 //  i. aware of subscription funct; & 
 //  ii. tells it this func should be executed whenever the state changes.
-// Redux will execute the suscription function. Hence, there is no need to execute the func yourself.
+// Redux will execute the suscription function. Hence, there is no need to execute the func.
 // Similar to the reducer func, enough to just point at it.
 
-// Action
+// ACTION
 store.dispatch({ type: 'increment' }); // { counter: 2}
 // dispatch() is a method which dispatches an action.
 // This causes the reducer function to run again.
