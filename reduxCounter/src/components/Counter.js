dc45276882142400a5eb1fd9f2a2769158d066cd
wteo <wendyteo.wy@gmail.com>
2022-06-25 18:01:34 +0800

    Suscribed Counter component via useSelector() hook

--- reduxCounter/src/components/Counter.js
+++ reduxCounter/src/components/Counter.js
@@ -1,12 +1,22 @@
+import { useSelector, connect } from 'react-redux';
+// useSelector - allows you to select a specific data in the store
+// connect - used for class-based components
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
+
+  // Suscription
+  const counter = useSelector(state => state.counter);
+  // need to pass a function within the paramenter of useSelector
+  // with useSelector(), React-Redux automatically sets up a suscription to the store for this component
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
