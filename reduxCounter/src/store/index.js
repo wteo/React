ba86a11bf834fcebd5ed937c113890c940626848
wteo <wendyteo.wy@gmail.com>
2022-06-25 16:56:28 +0800

    Set up ReduxCounter app

---
+++ reduxCounter/src/store/index.js
@@ -0,0 +1,23 @@
+import Redux from 'react';
+
+// Reducer
+const counterReducer = (state = { counter: 0 }, action) => {
+    if (action.type === 'increment') {
+        return {
+            counter: state.counter + 1
+        }
+    }
+    if (action.type === 'decrement') {
+        return {
+            counter: state.counter - 1
+        }
+    }
+    return state;
+};
+
+// Store
+const store = Redux.createStore(counterReducer);
+
+export default store;
+// This allows other components to have access to the store.
+// The subscription and dispatch will be placed in various components.
