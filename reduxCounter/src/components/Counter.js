b8e552cec727711b4d1788fb4a852a8416e0b1e5
wteo <wendyteo.wy@gmail.com>
2022-06-26 10:04:40 +0800

    Added payload to dispatch action increaseHandler

--- reduxCounter/src/components/Counter.js
+++ reduxCounter/src/components/Counter.js
@@ -16,6 +16,10 @@ const Counter = () => {
     dispatch({ type: 'increment' });
   }
 
+  const increaseHandler = () => {
+    dispatch({ type: 'increase', amount: 10 });
+  }
+
   const decrementHandler = () => {
     dispatch({ type: 'decrement' });
   }
@@ -26,6 +30,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 10</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
